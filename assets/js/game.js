9cad76514099cc7450a18da16fd450e5e9c3841f
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2016-11-22 13:59:14 -0500

    updates game logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,51 +4,75 @@ let gameState = {
     wins: 0,
     remainingGuesses: 10,
     status: "",
-    lettersUsed: [],
-
-    updateState: function(prop, val) {
-        this[prop] = val;
-    }
+    lettersUsed: []
 };
 
 function game(guess) {
 
-    //Initialise the game
+    // Step 1  Initialise the game----------------------------------------------
     document.getElementById("status-bar").textContent = "Ok. Guess a letter.";
     document.getElementById("wins").textContent = " " + gameState.wins;
 
-    //Get a random word from the words Array
+    // Step 2 Get a random word from the words Array if currentWord is empty----
     if (gameState.currentWord.length === 0) {
         let wd = getWord();
-
         gameState.currentWord = wd.split("");
+
+        //offset first guess of game
+        ++gameState.remainingGuesses;
     }
 
-    //Paint the game area with the letter placeholders
+    // Step 3 Paint the game area with the letter placeholders------------------
     paintWord();
+    --gameState.remainingGuesses;
 
-    //Test the users guesses
-    console.log(guess);
+    // Step 4  Test the users guesses if they guessed it already push it to the list-----
+    console.log("Guessed: " + guess);
     if (gameState.lettersUsed.indexOf(guess) !== -1) {
         document.getElementById("status-bar").textContent = "You guessed this already. Try again."
     } else {
         gameState.lettersUsed.push(guess);
+
+        //FIX ME to include each occurance of correct guess
+        if (gameState.currentWord.indexOf(guess) !== -1) {
+            for (var i = 0; i < gameState.formatedWord.length; i++) {
+                if (gameState.currentWord[i] === guess) {
+                    gameState.formatedWord[i] = guess;
+                }
+            }
+            // gameState.formatedWord[gameState.currentWord.indexOf(guess)] = guess;
+        } else {
+            document.getElementById("status-bar").textContent = "Nope. Guess again. " + gameState.remainingGuesses + " tries left.";
+        }
     }
 
-    //FIX ME to include each occurance of correct guess
-    if (gameState.currentWord.indexOf(guess) !== -1) {
-        gameState.formatedWord[gameState.currentWord.indexOf(guess)] = guess;
+// Step 5  Repaint the game area with updated list--------------------------
+paintWord();
 
-    } else {
-        console.log("nope");
+// Update when user guessed the word----------------------------------------
+if (gameState.currentWord.join(" ") === gameState.formatedWord.join(" ")) {
+    gameState.status = "winner";
+}
 
+//Check if user has won-----------------------------------------------------
+checkState();
+}
+
+function checkState() {
+    if (gameState.status === "winner") {
+        // if winner reset everything
+        document.getElementById("status-bar").textContent = "Guess a letter to play again.";
+        document.getElementById("current-word").textContent = "YOU WIN!!"
+        gameState.currentWord = [];
+        gameState.formatedWord = [];
+        gameState.status = "";
+        gameState.remainingGuesses = 10;
+        ++gameState.wins;
     }
-
-    paintWord();
 }
 
 function getWord() {
-    let words = ["fry", "leela", "cigars", "beer", "jukebox", "chocolate"];
+    let words = ["fry", "leela", "cigars", "beer", "king", "ladies", "money"];
     let num = Math.floor(Math.random() * words.length);
     for (var i = 0; i < words[num].length; i++) {
         gameState.formatedWord.push(" ___ ");
@@ -59,7 +83,7 @@ function getWord() {
 function paintWord() {
     let str = "";
 
-    document.getElementById("current-word").textContent = " " + gameState.formatedWord.join(" ");
+    document.getElementById("current-word").textContent = " " + gameState.formatedWord.join("  ");
 
     for (var i = 0; i < gameState.lettersUsed.length; i++) {
         str += " " + gameState.lettersUsed[i];
